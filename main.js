f65185de4cb66649111cf29f4c601a7005d83366
javeriagithub <163407795+javeriagithub@users.noreply.github.com>
2024-03-15 00:15:18 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,97 @@
+//Arthmatic opertor
+let num1 = 5
+let num2 = 10
+console.log(num1 + num2);
+
+let bag  = 2
+let apple = 3
+console.log(bag + apple);
+
+//subract opertor
+let alishastudents = 10
+let toobastudents = 20
+console.log(alishastudents - toobastudents);
+
+let x1= 10
+let y1 = 5
+console.log(x1-y1);
+
+let z = 20
+console.log(z-=5);
+
+//divd opertor
+
+let totalworker = 100
+let workerleave = 45
+console.log(totalworker/ workerleave);
+
+let a1 = 225
+let b1 = 5
+console.log(a1/b1);
+
+let d = 27
+console.log(d/=3); 
+
+//moduls 
+import studentlogin from "./first";
+import teacherlogin from "./second";
+console.log("studentlogin()");
+console.log(teacherlogin()); 
+ 
+const bag1 = 20
+const apple1 = 10
+console.log(bag1/apple1);
+
+
+let z1 = 50
+let y2 = 13
+console.log(z1/y2);
+
+let d1 = 60
+console.log(d1/=10)
+
+//expontional
+
+const num3 = 5
+const num4 = 2
+console.log(num3 ** num4);
+
+let a2=2
+let b2=0
+console.log(a2 ** b2);
+ let x2=-2
+ let y3=4
+ console.log(x2**y3);
+
+ let d2=4
+
+ console.log(d2**2);
+
+ //comparsion methode
+
+
+ let x3 =5
+ let y5=5
+ console.log(5==5)
+
+ let alisha2= 'tomorrow we will go to walk'
+let saima2="tomorrow we will go to walk"
+console.log(alisha2==saima2);
+let alisha3= 'tomorrow we will go to walk'
+let saima3="tomorrow we will go to walk"
+console.log(alisha3===saima3);
+
+let x4=5
+let y6=5
+console.log(x4 != y6);
+let a3=5
+let b3= 6
+console.log(5<6);
+console.log(5<=5);
+
+
+
+
+
+
+
